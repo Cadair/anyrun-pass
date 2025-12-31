52c0413e78cb9092c6378ef4624f09f7d663b063
Stuart Mumford <stuart@cadair.com>
2025-12-31 16:33:58 +0000

    More logging and error handling

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,31 @@ use nucleo_matcher::{Config as nuConfig, Matcher};
 use serde::Deserialize;
 use std::env::var;
 use std::fs;
+use std::io::Error;
 use std::path::{Path, PathBuf};
 use std::process::Command;
-use std::io::Error;
 
 #[derive(Deserialize, Debug)]
 struct Config {
     minimum_length: usize,
     max_results: usize,
-    // store_path: PathBuf,
+    store_path: PathBuf,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Config {
             minimum_length: 3,
-            // store_path:
+            store_path: {
+                if let Ok(env_path) = &var("PASSWORD_STORE_DIR") {
+                    PathBuf::from(env_path)
+                } else {
+                    let mut store_path = PathBuf::new();
+                    store_path.push(&var("HOME").unwrap());
+                    store_path.push(".password-store");
+                    store_path
+                }
+            },
             max_results: 10,
         }
     }
@@ -46,11 +55,9 @@ fn init(config_dir: RString) -> State {
         }
     };
 
-    // TODO: Also config this
-    let mut store_path = PathBuf::new();
-    store_path.push(&var("HOME").unwrap());
-    store_path.push(".password-store");
-    let mut file_glob_pattern = store_path.to_path_buf();
+    eprintln!("[pass] Loading entries from store: {}", config.store_path.to_string_lossy());
+
+    let mut file_glob_pattern = config.store_path.to_path_buf();
     file_glob_pattern.push("**");
     file_glob_pattern.push("*.gpg");
     let paths = glob(&file_glob_pattern.to_string_lossy()).unwrap();
@@ -61,15 +68,16 @@ fn init(config_dir: RString) -> State {
         match entry {
             Ok(path) => {
                 // Ignore any git files
-                if path.starts_with(Path::join(&store_path, ".git")) {
+                if path.starts_with(Path::join(&config.store_path, ".git")) {
                     continue;
                 }
-                let relative_path = path.strip_prefix(&store_path).unwrap();
+                let relative_path = path.strip_prefix(&config.store_path).unwrap();
                 pass_files.push(String::from(relative_path.to_string_lossy()));
             }
-            Err(e) => println!("{:?}", e),
+            Err(e) => eprintln!("[pass] Skipping file {:?} - {}", e.path(), e.error()),
         }
     }
+    eprintln!("[pass] Found {} entries in store.", pass_files.len());
     State { pass_files, config }
 }
 
@@ -128,13 +136,19 @@ fn get_matches(input: RString, state: &mut State) -> RVec<Match> {
     matches.into()
 }
 
-fn shell_out_to_pass(filename: &str) -> Result<String, Error> {
+fn shell_out_to_pass(store_path: &str, filename: &str) -> Result<String, Error> {
     let output = Command::new("pass")
+        .env("PASSWORD_STORE_DIR", store_path)
         .arg(filename)
         .output()?;
     let output_str = String::from_utf8(output.stdout).unwrap();
     match output_str.lines().next() {
-        None => return Err(std::io::Error::new(std::io::ErrorKind::UnexpectedEof, "No output")),
+        None => {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::UnexpectedEof,
+                "No output",
+            ))
+        }
         Some(first_line) => return Ok(first_line.to_string()),
     }
 }
@@ -142,23 +156,24 @@ fn shell_out_to_pass(filename: &str) -> Result<String, Error> {
 #[handler]
 fn handler(selection: Match, state: &State) -> HandleResult {
     let ROption::RSome(id) = selection.id else {
-        eprintln!("[pass] Unable to open {}", selection.title);
-        return HandleResult::Close
+        eprintln!("[pass] Internal Error - Unable to open {}", selection.title);
+        return HandleResult::Close;
     };
     let id: usize = id as usize;
     let relative_path = &state.pass_files[id];
-    let secret_name = relative_path.strip_suffix(".gpg").unwrap_or_else(|| { relative_path });
+    let secret_name = relative_path
+        .strip_suffix(".gpg")
+        .unwrap_or_else(|| relative_path);
 
-    println!("Opening Secret: {}", secret_name);
-    match shell_out_to_pass(secret_name) {
+    println!("[pass] Reading password from store: {}", secret_name);
+    match shell_out_to_pass(&state.config.store_path.to_string_lossy(), secret_name) {
         Err(e) => {
-            eprintln!("{}", e);
-            return HandleResult::Close
-        },
+            eprintln!("[pass] Failed to read password from store: {}", e);
+            return HandleResult::Close;
+        }
         Ok(password) => {
             eprintln!("{}", password);
-            return HandleResult::Copy(RVec::from(password.into_bytes()))
-        },
-
+            return HandleResult::Copy(RVec::from(password.into_bytes()));
+        }
     }
 }
